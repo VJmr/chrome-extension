71f583dd1d5ba3dc8eb05eac5751176ad5316c96
VJmr <jaganvudumula@gmail.com>
2019-03-18 23:16:46 +0530

    initial base files

---
+++ app.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var filterButton = document.getElementById('filterBtn');
+  filterButton.addEventListener('click', function() {
+    alert('Hi');
+  }, false);
+}, false);
